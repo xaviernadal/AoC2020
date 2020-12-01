0a5918e9ed2567dd9781439d0a46e802edd97b0c
xavi_nadal <xavinadal14@gmail.com>
2020-12-01 11:44:56 +0100

    day1 final

--- AoC2020/day1/Day1.py
+++ AoC2020/day1/Day1.py
@@ -1,14 +1,20 @@
+with open("day1/input.txt") as f:
+    number = f.readlines()
+    number = list(map(int, number))
+
 def part1():
-    with open("day1/input.txt") as f:
-        number = f.readlines()
-        number = list(map(int, number))
-        print(number)
+    for i in number:
+        for j in number:
+            if i + j == 2020:
+                return i * j
+
+def part2():
     for i in number:
         for j in number:
             for k in number:
-                if i + j + k== 2020:
+                if i + j + k == 2020:
                     return i * j * k
-
 
 if __name__ == "__main__":
     print(part1())
+    print(part2())
